4136ea4af2aee8fc1e08518f5cf50d1432187168
Calvin <calvinku96@gmail.com>
2023-12-12 08:21:40 +0800

    update

--- plugins.lua
+++ plugins.lua
@@ -59,6 +59,24 @@ local plugins = {
       local override = {
         mapping = {
           ["<C-j>"] = cmp.mapping(function(fallback)
+            if snip.expand_or_jumpable() then
+              vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
+            elseif cmp.visible() then
+              cmp.select_next_item { behaviour = cmp.SelectBehavior.Insert }
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<C-k>"] = cmp.mapping(function(fallback)
+            if snip.jumpable(-1) then
+              vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
+            elseif cmp.visible() then
+              cmp.select_prev_item { behaviour = cmp.SelectBehavior.Insert }
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<C-n>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
               cmp.select_next_item { behaviour = cmp.SelectBehavior.Insert }
             elseif snip.expand_or_jumpable() then
@@ -67,10 +85,24 @@ local plugins = {
               fallback()
             end
           end, { "i", "s" }),
-          ["<C-k>"] = cmp.mapping(function(fallback)
+          ["<C-p>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
               cmp.select_prev_item { behaviour = cmp.SelectBehavior.Insert }
             elseif snip.jumpable(-1) then
+              vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<tab>"] = cmp.mapping(function(fallback)
+            if snip.expand_or_jumpable() then
+              vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<S-tab>"] = cmp.mapping(function(fallback)
+            if snip.jumpable(-1) then
               vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
             else
               fallback()
